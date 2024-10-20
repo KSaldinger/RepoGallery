32b06b0e6a845e06fef8e8687e46f99a72752532
katy_s <ksaldinger@gmail.com>
2024-10-20 15:08:06 -0700

    Updated js to complete repo list for repo galery

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const overview = document.querySelector(".overview");
 // username
 const username = "KSaldinger";
 
+///  Repos list ///
+const reposList = document.querySelector(".repo-list");
+
 /////  GitHub profile  ///
 const gitProfile = async function() {
     const response = await fetch(`https://api.github.com/users/${username}`);
@@ -23,10 +26,31 @@ const userInfo = function (data) {
     </figure>
     <div>
         <p><strong>Name:</strong> ${data.name} </p>
-        <p><storn>Bio:</strong> ${data.bio}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>    
     `;
     overview.append(div);
+showRepos();
+};
+
+////// fetch the repos from gitHub ///////
+const showRepos = async function () {
+    const res = await fetch(` https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await res.json();
+    //console.log(repoData);
+    displayRepos(repoData);
+};
+showRepos();
+
+///// Lets see those repos displayed ////
+const displayRepos = function (repos) {
+    
+    for (repo of repos) {
+        let li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        reposList.append(li);
+    };
 };
